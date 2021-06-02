abd8f25dbee3973006ef7e49b9197999f169b0af
akasifislam <aseasifislam@gmail.com>
2021-06-02 13:44:11 +0600

    article page

--- src/Composable/useArticle.js
+++ src/Composable/useArticle.js
@@ -7,17 +7,28 @@ export function useArticle() {
 
     let articles = ref({
         data: [],
+        links: {},
+        meta: {},
 
     });
     let loading = ref(false);
 
 
-    function fetchArticles() 
+    function fetchArticles(params = {})
+
     {
+        let { page=1 , showMore=false} = params;
         loading.value = true;
-        api.get('articles')
+        api.get('articles',{params:{page}})
         .then((response) =>{
-            articles.value = response.data;
+            if (showMore) {
+                articles.value = {
+                    ...response.data,
+                    data:[...articles.value.data,...response.data.data]
+                }
+            }else{
+                articles.value = response.data;
+            }
         })
         .finally(()=>loading.value=false);
     }
--- src/views/Articles/index.vue
+++ src/views/Articles/index.vue
@@ -5,9 +5,14 @@
   />
 
   <div class="container mx-auto px-4 sm:px-0 py-8 sm:py-12">
-    <Articles layout="grid" :show-view-all="false" />
+    <Articles layout="grid" :show-view-all="false" :articles="articles.data" />
     <div class="mt-8 sm:mt-12">
-      <button class="px-4 py-2 sm:px-6 bg-primary text-white rounded focus:outline-none">view more</button>
+      <button
+        class="px-4 py-2 sm:px-6 bg-primary text-white rounded focus:outline-none"
+        @click="showMoreArticle"
+      >
+        view more
+      </button>
     </div>
   </div>
 </template>
@@ -15,13 +20,31 @@
 <script>
 import InnerPageHero from "@/components/InnerPageHero";
 import Articles from "@/components/Articles";
+import { useArticle } from "@/Composable/useArticle.js";
 export default {
   components: {
     InnerPageHero,
     Articles,
   },
   props: {},
-  setup() {},
+  setup() {
+    let { fetchArticles, loading, articles } = useArticle();
+
+    fetchArticles();
+
+    function showMoreArticle() {
+      fetchArticles({
+        showMore: true,
+        page: articles.value.meta.current_page + 1,
+      });
+    }
+
+    return {
+      articles,
+      loading,
+      showMoreArticle,
+    };
+  },
 };
 </script>
 
